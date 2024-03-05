de93af0f494c7ef5bf0c63ca5bf6bbb44dba251c
Facundo Diaz <facucristian@gmail.com>
2024-03-05 09:41:41 -0300

    Inicio de parte logica (falta responsive)

---
+++ script.js
@@ -0,0 +1,16 @@
+var btnEncriptar = document.querySelector(".boton-encriptar");
+var btnDesencriptar = document.querySelector(".boton-desencriptar");
+
+btnEncriptar.onclick = encriptacion;
+
+function encriptacion(){
+}
+
+function ingresoTexto{
+    var texto = document.querySelector("#ingreso-texto");
+    return texto.value;
+}
+
+function cambioPanelDerecho{
+
+}
